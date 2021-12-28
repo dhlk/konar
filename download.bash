12bf147d0d3efd322eed9de7aacbed9643f6a11c
dhlk <dhlk@users.noreply.github.com>
2021-12-28 00:03:25 -0500

    initial commit

---
+++ download.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+uriformat='https://secure.runescape.com/m=%s/index_lite.ws?player=%s'
+retrieval="$(date --utc --rfc-3339=seconds)"
+
+if [ ! -s "$1" ]; then
+	echo "must supply a character queue" 1>&2
+	exit 1
+fi
+
+jq -r 'keys[]' "$1" | while read -r key; do
+	jq -r '.[$key][]' "$1" --arg key "$key" | while read -r character; do
+		mkdir -p "$key/$character"
+		curl -s "$(printf "$uriformat" "$key" "$character")" > "$key/$character/$retrieval.csv"
+	done
+done
